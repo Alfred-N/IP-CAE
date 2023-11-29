149a7ee79654df81d345159519795887525b689d
Alfred <alfredn@kth.se>
2023-11-29 15:14:22 +0100

    remove commented old code

--- src/fs_datasets.py
+++ src/fs_datasets.py
@@ -114,15 +114,9 @@ def download_isolet(root):
     path = os.path.join(root, "isolet")
     download_zip("https://archive.ics.uci.edu/static/public/54/isolet.zip", path)
 
-    #old, reliant on linux package 'uncompress'
-    # subprocess.run(["uncompress", os.path.join(path, "isolet1+2+3+4.data.Z")])
-    # subprocess.run(["uncompress", os.path.join(path, "isolet5.data.Z")])
-
-    # use this package instead, and then also save the files (assuming as .data files?)
     uncompressed_data_part_1 = unlzw3.unlzw(Path(os.path.join(path, "isolet1+2+3+4.data.Z")))
     uncompressed_data_part_2 = unlzw3.unlzw(Path(os.path.join(path, "isolet5.data.Z")))
 
-    # Optionally, save the decompressed data files if needed
     with open(os.path.join(path, "isolet1+2+3+4.data"), 'wb') as data_file:
         data_file.write(uncompressed_data_part_1)
 
