b76262c5df1115af9b47fc63f3eeb51a5c81a030
Alfred-N <alfredn@kth.se>
2024-03-26 16:55:45 +0100

    log the norm of logits/"log alpha" gradients

--- src/models/gumbel_distrib.py
+++ src/models/gumbel_distrib.py
@@ -484,6 +484,14 @@ class GumbelDistribution(pl.LightningModule):
         logits = torch.log(pi)
         return pi, logits, pi_raw
 
+    def log_gradient_norm(self, grad, name="grad_norm_logits"):
+        # Calculate the norm of the gradient
+        grad_norm = grad.norm(2).item()
+        # Log using the PyTorch Lightning `log` method
+        self.logger.experiment.log(
+            {name: grad_norm, "global_step": self.trainer.global_step}
+        )
+
     def batch_sample_joint(
         self,
         num_batches,
@@ -499,6 +507,9 @@ class GumbelDistribution(pl.LightningModule):
         no_gumbel_noise=False,
     ):
         pi, logits, logits_raw = self.get_pi(eps=eps)
+
+        if self.training:
+            logits.register_hook(lambda grad: self.log_gradient_norm(grad))
 
         distrib_dict = {"num_categories": pi.shape[1], "current_pi": pi}
         if ret_GJS:
